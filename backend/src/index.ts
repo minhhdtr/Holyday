ad3a6d7b3197e29b5024671423b4b1918f8e5cc4
Tran Duc Minh <minhhdtr@duc-minhhs-MacBook.local>
2025-06-16 13:03:19 +0700

    initial commit

---
+++ backend/src/index.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api', async(req: Request, res: Response) => {
+  // res.send('Hello World!');
+  res.json({message: 'Hello World!'});
+});
+
+app.listen(7000, () => {
+  console.log('Server is running on http://localhost:7000');
+});
---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+
+function App() {
+  return (
+    <>
+      <p className='bg-blue-600 text-9xl text-red-500 text-center'>Hello World</p>
+    </>
+  )
+}
+
+export default App
