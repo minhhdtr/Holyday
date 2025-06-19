ee45fea0be9123e94673691f5a49bfdb89d71f19
Tran Duc Minh <minhhdtr@duc-minhhs-MacBook.local>
2025-06-19 10:45:09 +0700

    test

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,20 +3,28 @@ import cors from 'cors';
 import 'dotenv/config';
 import cookieParser from 'cookie-parser';
 import mongoose from 'mongoose';
+import path from 'path';
 
 import authRoutes from './routes/auth';
 import userRoutes from './routes/users';
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
+  // .then(() => {
+  //   console.log('Connected to database: ', process.env.MONGODB_CONNECTION_STRING);
+  // });
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
+
+app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
